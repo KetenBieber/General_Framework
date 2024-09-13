d53719a0316916fe8b1f4709bbe1dad6bebc8d1a
KetenBieber <2863861004@qq.com>
2024-09-13 17:37:54 +0800

    Keten让你没问题

--- app/LogTest/logtest.c
+++ app/LogTest/logtest.c
@@ -7,12 +7,14 @@
 
 int LogTestFunc(void)
 {
+    int b =0;
     PrintLog("LogTestFunc\n");
     return 0;
 }
 
 int LogTestFunc1(void)
 {
+    int c =0;
     PrintLog("LogTestFunc1\n");
     return 0;
 }   
