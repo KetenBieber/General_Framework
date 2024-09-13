0b46b564f09bb31ede33cb2b620be24a285cbcd3
Ray <1911542613@qq.com>
2024-09-13 22:33:45 +0800

    随便搞点事情

--- app/LogTest/logtest.h
+++ app/LogTest/logtest.h
@@ -1,22 +1,23 @@
 /**
  * @file logtest.h
  * @author Keten (2863861004@qq.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-09-13
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  * @attention :
  * @note :
  * @versioninfo :
  */
 
-#ifndef LOGTEST_H 
-#define LOGTEST_H 
+#ifndef LOGTEST_H
+#define LOGTEST_H
 
 #ifdef __cplusplus
-extern "C"{
+extern "C"
+{
 #endif
 
 /*----------------------------------include-----------------------------------*/
@@ -24,24 +25,27 @@ extern "C"{
 /*-----------------------------------macro------------------------------------*/
 #define LOG_TEST_TASK_STACK_SIZE 1024
 #define LOG_TEST_TASK_PRIORITY 5
-/*----------------------------------typedef-----------------------------------*/
-typedef int (*log_test_func)(void);
-typedef int (*log_test_func1)(void);
-typedef int (*log_test_func2)(void);
-typedef int (*log_test_func666)(void);
-typedef int (*log_test_func777)(void);
-/*----------------------------------variable----------------------------------*/
+    /*----------------------------------typedef-----------------------------------*/
+    typedef int (*log_test_func)(void);
+    typedef int (*log_test_func1)(void);
+    typedef int (*log_test_func2)(void);
+    typedef int (*log_test_func666)(void);
+    typedef int (*log_test_func777)(void);
+    /*----------------------------------variable----------------------------------*/
 
-/*-------------------------------------os-------------------------------------*/
+    /*-------------------------------------os-------------------------------------*/
 
-/*----------------------------------function----------------------------------*/
+    /*----------------------------------function----------------------------------*/
 
-/*------------------------------------test------------------------------------*/
-int a =1;
-int b =2;
-int c =3;
+    /*------------------------------------test------------------------------------*/
+    int a = 1;
+
+    int b = 2;
+
+    int c = 3;
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif	/* LOGTEST_H */
+#endif /* LOGTEST_H */
---
+++ app/Upper/Upper.c
@@ -0,0 +1,9 @@
+#include "upper.h"
+
+int uppper_a;
+
+void Upper_Init()
+{
+    BSPLogInit();
+    LOGINFO("Upper_Init");
+}
