e5068a1a720c9fed2785811ec5e246b2911eab61
KetenBieber <2863861004@qq.com>
2024-09-20 19:20:16 +0800

    把那些打补丁时遇到错误生成的中间文件全部删除了，现在看起来更加清爽了

---
+++ build/logtest.d
@@ -0,0 +1,23 @@
+build/logtest.o: App/logtest/logtest.c App/logtest/logtest.h \
+ Bsp/log/bsp_log.h Middlewares/Third_Party/SystemView/SEGGER/SEGGER_RTT.h \
+ Middlewares/Third_Party/SystemView/SEGGER/../Config/SEGGER_RTT_Conf.h \
+ Middlewares/Third_Party/SystemView/Config/SEGGER_RTT_Conf.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/freertos.h \
+ Core/Inc/FreeRTOSConfig.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+App/logtest/logtest.h:
+Bsp/log/bsp_log.h:
+Middlewares/Third_Party/SystemView/SEGGER/SEGGER_RTT.h:
+Middlewares/Third_Party/SystemView/SEGGER/../Config/SEGGER_RTT_Conf.h:
+Middlewares/Third_Party/SystemView/Config/SEGGER_RTT_Conf.h:
+Middlewares/Third_Party/FreeRTOS/Source/include/freertos.h:
+Core/Inc/FreeRTOSConfig.h:
+Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
