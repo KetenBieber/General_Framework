4fc39f36e74d20d0582d316be13ddbaa9867f31e
Feng Zihan <3123000807@mail2.gdut.edu.cn>
2025-01-12 18:02:15 +0800

    嘻嘻zzx傻逼

--- Module/movebase/tracking.cpp
+++ Module/movebase/tracking.cpp
@@ -25,6 +25,7 @@ uint8_t Yaw_Adjust(PID_t *yaw_pid,float Target_Yaw,float Current_Yaw,float measu
     if(Current_Yaw*Target_Yaw >= 0)
     {
         temp_yaw_error = Target_Yaw - Current_Yaw;
+        int  cnm = 666;
     }
     else
     {
